0a56c03be19636c0c61605fe4d40a441128e9644
feyre macbook <feyremacbook@MacBook-Pro-2.local>
2024-09-25 22:09:43 -0400

    revised2

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_WINDOWS 100 // You can adjust this as needed
+#define MAX_WINDOWS 100 // Maximum number of windows that can be handled
 
 typedef struct
 {
@@ -26,48 +26,38 @@ int find_window(int id)
 }
 
 // Open a new window, bring it into focus
-int open_window(int id)
+void open_window(int id)
 {
     // Shift all windows to the right to make room at the front
     for (int i = num_windows; i > 0; i--)
     {
         windows[i] = windows[i - 1];
     }
-
     // Add new window at the front
     windows[0].id = id;
     num_windows++;
-
-    // Return the ID of the currently focused window
-    return windows[0].id;
 }
 
 // Switch focus to an existing window
-int switch_window(int id)
+void switch_window(int id)
 {
     int index = find_window(id);
-
     if (index != -1)
     {
-        // Shift all windows to the right, bring selected window to front
+        // Bring selected window to the front
         Window temp = windows[index];
         for (int i = index; i > 0; i--)
         {
             windows[i] = windows[i - 1];
         }
         windows[0] = temp;
-
-        // Return the ID of the currently focused window
-        return windows[0].id;
     }
-    return -1; // If switch failed
 }
 
 // Close an existing window
-int close_window(int id)
+void close_window(int id)
 {
     int index = find_window(id);
-
     if (index != -1)
     {
         // Remove the window by shifting others to the left
@@ -76,45 +66,39 @@ int close_window(int id)
             windows[i] = windows[i + 1];
         }
         num_windows--;
-
-        // If there are no windows left, terminate
-        if (num_windows == 0)
-        {
-            exit(0);
-        }
-
-        // Return the ID of the currently focused window
-        return windows[0].id;
     }
-    return -1; // If close failed
 }
 
 int main(void)
 {
-    char command[10];
-    int id;
+    char command[10]; // Command string
+    int id;           // Window ID
 
     // Read input until EOF
     while (scanf("%s %d", command, &id) == 2)
     {
-        int focused_id = -1; // To hold the ID of the currently focused window
         if (strcmp(command, "open") == 0)
         {
-            focused_id = open_window(id);
+            open_window(id);
         }
         else if (strcmp(command, "switch") == 0)
         {
-            focused_id = switch_window(id);
+            switch_window(id);
         }
         else if (strcmp(command, "close") == 0)
         {
-            focused_id = close_window(id);
+            close_window(id);
         }
 
         // Print the ID of the currently focused window
-        if (focused_id != -1)
+        if (num_windows > 0)
         {
-            printf("%d\n", focused_id);
+            printf("%d\n", windows[0].id);
+        }
+        else
+        {
+            // Exit if there are no windows left open
+            exit(0);
         }
     }
 
